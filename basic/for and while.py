18594de55f5b8441e9719a21b11576bcb3d1c503
macgyver2008 <88918816+macgyver2008@users.noreply.github.com>
2021-12-04 12:40:23 +0900

    Create for and while.py

---
+++ basic/for and while.py
@@ -0,0 +1,8 @@
+#for 사용법  for (변수) in range(반복수) :
+for i in range (10):    # 예
+#  while 사용법 while 조건 :
+while Ture: #( 무한반복)
+While A == B: #(A가 B강 같을때 반복)
+  
+  #while 도 if와 같이 조건에 not(조건) 이 가능하다
+              
